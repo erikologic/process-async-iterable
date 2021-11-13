9f9c1a464a2db10f183ef3357ceae87d6da4372a
Enrico Graziani <enrico.graziani@cazoo.co.uk>
2021-11-13 01:09:52 +0000

    chains can be reused

---
+++ test/acceptance/chains-can-be-reused.test.ts
@@ -0,0 +1,23 @@
+import { consume } from "../../src/consumers/consume";
+import { fromIterable } from "../../src/producers/from-iterable";
+import { filter } from "../../src/transformers/filter";
+import { map } from "../../src/transformers/map";
+
+test("chains can be reused", async () => {
+  const add3 = map((x: number) => x + 3);
+  const filterEven = filter((x: number) => x % 2 === 0);
+  const add3AndFilterEven = (source: AsyncIterable<number>) =>
+    consume(filterEven(add3(source)));
+
+  const array1 = [1, 2, 3, 4];
+  const source1 = fromIterable(array1);
+  const results1 = await add3AndFilterEven(source1);
+  const expectation1 = [4, 6];
+  expect(results1).toEqual(expectation1);
+
+  const array2 = [21, 22, 23, 24];
+  const source2 = fromIterable(array2);
+  const results2 = await add3AndFilterEven(source2);
+  const expectation2 = [24, 26];
+  expect(results2).toEqual(expectation2);
+});
